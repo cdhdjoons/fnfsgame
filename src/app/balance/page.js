3f5be852fb2085f6eea506af44d16a78273592d4
DevCrypto187 <crypto_dev@marzmain.io>
2025-02-12 03:27:28 +0900

    fnfs claim layout change

--- src/app/balance/page.js
+++ src/app/balance/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" w-full h-full">
-      <div className=" w-full h-full max-w-[500px] relative flex flex-col justify-evenly bg-cover bg-no-repeat " >
-        <div className=" bg-mainRed w-full py-[5vmax] flex justify-center items-center relative">
+      <div className=" w-full h-full max-w-[500px] relative flex flex-col justify-end pb-[10vmax] sm:pb-[3vmax] bg-cover bg-no-repeat " >
+        <div className=" bg-mainRed w-full py-[5vmax] flex justify-center items-center absolute top-0">
           <div className="w-[30vmax] max-w-[295px] aspect-[295/183] relative ">
             <Image
               src="/image/fnfsLogo.png"
